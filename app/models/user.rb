2d45933b0e9ccbcf600161227cda25ba3ec54175
takaaa220 <taaaaaaaaaa220@gmail.com>
2018-09-30 19:45:44 +0900

    finish tutorial6

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
   validates :email, {presence: true, uniqueness: {case_sensitive: false},
                     length: {maximum: 140},
                     format: {with: VALID_EMAIL_REGEX}}
+
+  has_secure_password
+  validates :password, {presence: true, length: {minimum: 6}}
+
 end
