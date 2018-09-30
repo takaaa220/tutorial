576dbe86d21e04e5b6fa4ac35e92b51a40ab1cdc
takaaa220 <taaaaaaaaaa220@gmail.com>
2018-09-30 19:16:42 +0900

    before 6.3

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  before_save {email.downcase!}
   validates :name, {presence: true, length: {maximum: 50}}
-  validates :email, {presence: true, length: {maximum: 140}}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, {presence: true, uniqueness: {case_sensitive: false},
+                    length: {maximum: 140},
+                    format: {with: VALID_EMAIL_REGEX}}
 end
