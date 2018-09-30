db89800c8d65c9b677201b3b442fcaa457288ac7
takaaa220 <taaaaaaaaaa220@gmail.com>
2018-09-30 17:39:48 +0900

    change link path

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'static_pages/about'
-  get 'static_pages/help'
-  get 'static_pages/contact'
+  get 'about' => "static_pages#about"
+  get 'help' => "static_pages#help"
+  get 'contact' => "static_pages#contact"
 
   root 'static_pages#home'
 
