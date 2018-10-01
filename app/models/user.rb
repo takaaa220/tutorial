1b2d0e7cca11dc3dde6338705e2035cc89776ecd
takaaa220 <taaaaaaaaaa220@gmail.com>
2018-10-01 19:07:16 +0900

    ログイン時のレイアウトの変更に対するテストを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,10 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, {presence: true, length: {minimum: 6}}
 
+  # Fixture 作成のためのメソッド
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                        BCrypt::Engine.cost # costとはハッシュを計算するための計算コスト
+    BCrypt::Password.create(string, cost: cost) # string で渡されたパスワードを暗号化
+  end
 end
