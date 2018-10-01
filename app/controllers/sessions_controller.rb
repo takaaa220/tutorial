1f57c0241faed051fa5cb64f2f924c75ee4dec62
takaaa220 <taaaaaaaaaa220@gmail.com>
2018-10-01 17:39:12 +0900

    define current_user and session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,21 @@
 class SessionsController < ApplicationController
+
   def new
+  end
+
+  def create
+    user = User.find_by(email: params[:sessions][:email].downcase)
+    if user && user.authenticate(params[:sessions][:password])
+      # ユーザーログイン後にユーザー情報のページにリダイレクトする
+      login user
+      flash[:success] = "ログインしました"
+      redirect_to user
+    else
+      flash.now[:danger] = 'アドレスまたはパスワードを確認してください' # 本当は正しくない
+      render 'new'
+    end
+  end
+
+  def destroy
   end
 end
