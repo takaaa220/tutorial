3ddb88296e340339fb8c4dc63a11b67da81360d9
takaaa220 <taaaaaaaaaa220@gmail.com>
2018-10-02 18:58:40 +0900

    assignsメソッドを用いて，Remember me boxテストを書き換えた

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,14 +4,13 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-      # ユーザーログイン後にユーザー情報のページにリダイレクトする
-      log_in user # 一時cookie に User_id 追加
+    @user = User.find_by(email: params[:session][:email].downcase)
+    if @user && @user.authenticate(params[:session][:password])
+      log_in @user # 一時cookie に User_id 追加
       # 署名付き永続cookieに暗号化したRemember_token，user_id を追加, もしくは永続的セッションの破棄
-      params[:session][:remember_me] == "1" ? remember(user) : forget(user)
+      params[:session][:remember_me] == "1" ? remember(@user) : forget(@user)
       flash[:success] = "ログインしました"
-      redirect_to user
+      redirect_to @user # ユーザーログイン後にユーザー情報のページにリダイレクトする
     else
       flash.now[:danger] = 'アドレスまたはパスワードを確認してください' # 次のページのみ表示
       render 'new'
