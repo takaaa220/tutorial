883e93e37c5df6959b1395d4577275f557248839
takaaa220 <taaaaaaaaaa220@gmail.com>
2018-10-04 14:00:56 +0900

    Userの一覧表示（ページネーションを含む）

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,6 +2,16 @@ require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
 
+  def setup
+    @user = users(:michael)
+    @other = users(:archer)
+  end
+
+  test "should redirect index when not logged in" do
+    get users_path
+    assert_redirected_to login_url
+  end
+
   test "should get new" do
     get signup_path
     assert_response :success
