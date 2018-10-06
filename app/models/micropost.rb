8324e80c00903f70e891b4809ef8bfbda9a710ca
takaaa220 <taaaaaaaaaa220@gmail.com>
2018-10-06 21:20:47 +0900

    micropostのアップロード画像にValidationを付与

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -7,4 +7,14 @@ class Micropost < ApplicationRecord
   mount_uploader :picture, PictureUploader
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
+  validate :picture_size # 独自のバリデーションを定義
+
+  private
+
+    # アップロードされた画像のサイズをバリデーションする
+    def picture_size
+      if picture.size > 5.megabytes
+        errors.add(:picture, "should be less than 5MB")
+      end
+    end
 end
