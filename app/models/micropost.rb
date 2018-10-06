517fe27b5058296276c71b58483277bcf329ab92
takaaa220 <taaaaaaaaaa220@gmail.com>
2018-10-06 21:03:55 +0900

    micropostに画像アップロード機能の追加

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -4,6 +4,7 @@ class Micropost < ApplicationRecord
   # ex) scope :desc, -> {order(created_at: :desc)} usage) Micropost.desc
   default_scope -> { order(created_at: :desc)}
 
+  mount_uploader :picture, PictureUploader
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
 end
