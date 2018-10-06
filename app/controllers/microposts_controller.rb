88b204fc9adcc3971960bfec5295e82cfd1fee37
takaaa220 <taaaaaaaaaa220@gmail.com>
2018-10-06 20:17:54 +0900

    つぶやきの削除機能の追加

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,5 +1,6 @@
 class MicropostsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
+  before_action :corrent_user, only: :destroy
 
   def create
     @micropost = current_user.microposts.build(micropost_params)
@@ -13,11 +14,19 @@ class MicropostsController < ApplicationController
   end
 
   def destroy
+    @micropost.destroy
+    flash[:success] = "つぶやきを削除しました"
+    redirect_to request.referrer || root_url
   end
 
   private
 
     def micropost_params
       params.require(:micropost).permit(:content)
+    end
+
+    def corrent_user
+      @micropost = current_user.microposts.find_by(id: params[:id])
+      redirect_to root_url if @micropost.nil?
     end
 end
