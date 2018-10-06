a03f40fac31c218af930c6230099ba3a102b9635
takaaa220 <taaaaaaaaaa220@gmail.com>
2018-10-06 18:38:47 +0900

    default scope を使ってmicropost を降順に並び替え

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,8 @@
 class Micropost < ApplicationRecord
   belongs_to :user
+  # ずっと(scopeを使えば任意のとき呼び出すことができる)
+  # ex) scope :desc, -> {order(created_at: :desc)} usage) Micropost.desc
+  default_scope -> { order(created_at: :desc)}
 
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
