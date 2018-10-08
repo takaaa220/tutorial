6eb87a7c1d6609c1353e4965b6cd06b78cf5de0f
takaaa220 <taaaaaaaaaa220@gmail.com>
2018-10-08 17:04:37 +0900

    フォローしているユーザ

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   has_many :active_relationships, class_name: "Relationship",
                           foreign_key: "follower_id",
                           dependent: :destroy
+  # has_many :followeds, through: :active_relationshipsがdefault
+  # user.followedsをuser.followingに変更
+  has_many :following, through: :active_relationships, source: :followed
 
   attr_accessor :remember_token, :activation_token, :reset_token # 暗号化前
 
@@ -81,6 +84,18 @@ class User < ApplicationRecord
   # 完全な実装は次章
   def feed
     Micropost.where("user_id=?", id)
+  end
+
+  def follow(other_user)
+    following << other_user
+  end
+  
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
   end
 
   private
